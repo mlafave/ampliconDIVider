1f29c561bd15a6c8133ec2acfa9b19e9e322c20d
mlafave <matt.lafave@gmail.com>
2014-03-26 15:36:54 -0400

    Added script to remove names of fragments with incorrect barcodes.

---
+++ sh/barcode_whitelist_compare.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BARCODE_REF=$1
+DETECTED_BARCODES=$2
+OUTPUT=$3
+
+source ~/.bashrc
+
+cut -f6 ${BARCODE_REF} | sort -u > ${BARCODE_REF}.whitelist
+
+gunzip -c ${DETECTED_BARCODES} \
+	| join -j 1 - ${BARCODE_REF}.whitelist \
+	| sort \
+	| gzip -c > ${OUTPUT}
+
+rm ${BARCODE_REF}.whitelist
+
+exit
